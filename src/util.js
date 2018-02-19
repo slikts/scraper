9097259b06db7884293178c72c5bd9827111d0fc
slikts <dabas@untu.ms>
2018-02-19 13:44:32 +0200

    Fix config

--- src/util.js
+++ src/util.js
@@ -1,5 +1,18 @@
+const toml = require(`toml`)
+const knex = require(`knex`)
+const fs = require(`fs`)
+
 const debug = require(`debug`)(require(`../package.json`).name)
+const config = toml.parse(fs.readFileSync(`${__dirname}/../config/config.toml`, `utf8`))
+
+debug(`config %o`, config)
+const getDb = (url = config.db.url) =>
+  knex({
+    client: `pg`,
+    connection: url,
+  })
 
 module.exports = {
   debug,
+  getDb,
 }
