eb0e0493a312c719848fa8491446c8e8654d691e
slikts <dabas@untu.ms>
2018-02-21 22:13:11 +0200

    Catch provider errors

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import Runner from './Runner'
 import { ProviderConstructor } from './Provider'
 import {
   log,
+  error,
   getDb,
   getProviderConstructors,
   ProviderConstructorData,
@@ -13,11 +14,15 @@ log(`config %o`, config)
 const main = async (db = getDb()) => {
   const { constructors, providerConfig } = await getProviderConstructors()
   for (const Provider of constructors) {
-    await new Runner({
-      provider: new Provider(providerConfig[Provider.name]),
-      db,
-      ...config.runner,
-    }).run()
+    try {
+      await new Runner({
+        provider: new Provider(providerConfig[Provider.name]),
+        db,
+        ...config.runner,
+      }).run()
+    } catch (err) {
+      error('provider error', err)
+    }
   }
   log(`all providers done`)
   // XXX why is this necessary
